4f653e61d6d5b664f8c1194900694195e1aedfb9
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-02-07 09:35:49 +0800

    create product controller and route

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create a Product</h1>
+    <form method="POST" action="">
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" placeholder="Name">
+        </div>
+        <div>
+            <label for="name">Qty</label>
+            <input type="text" name="qty" placeholder="Qty">
+        </div>        
+        <div>
+            <label for="name">Price</label>
+            <input type="text" name="price" placeholder="Price">
+        </div>     
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" placeholder="Description">
+        </div>        
+        <div>
+            <input type="submit" value="Save a New Product">
+        </div>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', [ProductController::class, 'index'])->name('products.index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('products.create');
